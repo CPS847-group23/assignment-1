71825cea3ad7e08a9e82955ed53b6b5cb20a97f2
matthewninja <matthewham4@gmail.com>
2020-01-29 16:41:11 -0500

    Added route for echo bot

--- shrimp_bot/routes/index.js
+++ shrimp_bot/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST shrimp bot. */
+router.post('/shrimp', function(req, res, next) {
+  res.json({"challenge":req.body.challenge});
+});
+
+router.post('/shrimp_echo', function(req, res, next) {
+  if (req.body.text === "secret") {
+    res.send("you found the secret function!");
+  } else {
+    res.send(req.body.text);
+  }
+});
+
 module.exports = router;
