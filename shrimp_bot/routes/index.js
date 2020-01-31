bd471453c0fc46d95426b700a90c3910f46db691
matthewninja <matthewham4@gmail.com>
2020-01-31 16:35:07 -0500

    Added weather feature skeleton

--- shrimp_bot/routes/index.js
+++ shrimp_bot/routes/index.js
@@ -4,6 +4,7 @@ let request = require('request');
 const shrimp = require('../config/shrimp_config');
 const shrimp_id = shrimp.id;
 const hook_url = shrimp.hook_url;
+let temp_weather = '{"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,7 +18,11 @@ router.post('/shrimp', function(req, res, next) {
   let user_msg = req.body.event.text;
   user_msg = user_msg.split("<@"+shrimp_id+">")[1];
   let reply_msg = {json: {"text": "<@" + user_id + "> "}};
-  if (user_msg.includes("secret")) {
+
+  if (is_city(user_msg)) {
+    // https://openweathermap.org/current#one
+    reply_msg.json.text += temp_weather;
+  } else if (user_msg.includes("secret")) {
     reply_msg.json.text += " you found the secret!!";
   } else {
     reply_msg.json.text += user_msg;
@@ -48,5 +53,7 @@ router.post('/shrimp_echo', function(req, res, next) {
     res.send(req.body.text);
   }
 });
-
+function is_city(text) {
+  return true;
+}
 module.exports = router;
